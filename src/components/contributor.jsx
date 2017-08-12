3d8d156ff5240737b81faabbba56a9f218757853
jewu <jeremy.wu@blackboard.com>
2017-08-13 02:39:43 +0800

    bug fix

--- src/components/contributor.jsx
+++ src/components/contributor.jsx
@@ -5,11 +5,11 @@ import {Card, Tag} from 'antd';
 export default class Contributor extends React.Component {
   render() {
     const user = this.props.user;
-    const color = ["pink", "red", "orange", "green", "cyan", "blue", "purple"]
+    const color = ["pink", "red", "orange", "green", "cyan", "blue", "purple"];
     return (
       <Card className="sha-contributor">
         <a target="_blank" href={"https://github.com/" + user.username}>
-          <img className="sha-logo" alt="" src={user.avatar_url || '/static/img/EastPerl.svg'}/>
+          <img className="sha-logo" alt="" src={user.avatar_url || '/static/imgs/EastPerl.svg'}/>
           <div className="sha-info">
             <div className="sha-nickname">{user.nickname}</div>
             <div className="sha-cup">罩杯: {user.cup}</div>
