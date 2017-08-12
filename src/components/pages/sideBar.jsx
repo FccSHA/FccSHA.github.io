054e7f12ea86a2eb35f4c2224a22207b9aade834
FccSHA <wqx591449570@gmail.com>
2017-08-12 15:37:24 +0800

    feature/About-Us

--- src/components/pages/sideBar.jsx
+++ src/components/pages/sideBar.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import {SideItem} from '../sideItem';
 import SideBarItems from '../../config/sidebar.config';
 
 export default class SideBar extends React.Component {
-  constructor() { 
+  constructor() {
     super();
   };
 
@@ -14,18 +13,43 @@ export default class SideBar extends React.Component {
         <img src="/static/EastPerl.svg" alt="EastPerl"/>
         <h2 className="sha-img-legend">FCC Shanghai</h2>
         <ul className="sha-side-bar">
-            {SideBarItems.map((v, i) => <SideItem title={v.title} icon={v.icon} to={v.to} key={i} />)}  
+          {SideBarItems.map((v, i) => <SideItem title={v.title} icon={v.icon} to={v.to} key={i}/>)}
         </ul>
 
         <div className="sha-social">
           <h3 className="sha-social-legend">Join Us</h3>
           <p className="sha-social-icons">
-            <img src="/static/github.svg" alt="" className="sha-social-icon"/>
-            <img src="/static/qq.svg" alt="" className="sha-social-icon"/>
-            <img src="/static/wechat.svg" alt="" className="sha-social-icon"/>
+            <a href="https://github.com/FccSHA/FccSHA.github.io" target="_blank">
+              <img src="/static/github.svg" alt="" className="sha-social-icon"/>
+            </a>
+            <a href="//shang.qq.com/wpa/qunwpa?idkey=cd476c93cb4faf805ba36b1be3a0daf0359d31628b14f654fc7a086dd911d05a"
+               target="_blank">
+              <img src="/static/qq.svg" alt="" className="sha-social-icon"/>
+            </a>
+            <a href="javascript: void(0)">
+              <img src="/static/wechat.svg"
+                   alt="WeChat"
+                   className="sha-social-icon"
+                   onMouseOver={this.onOver.bind(this)}
+                   onMouseOut={this.onLeave.bind(this)}/>
+            </a>
           </p>
         </div>
       </div>
     )
+  }
+
+  onOver($event) {
+    const div = document.createDocumentFragment();
+    const img = new Image();
+    img.src = '/static/group-owner.jpg';
+    img.className = 'sha-popover';
+    div.append(img);
+    $event.target.parentNode.append(div);
+  }
+
+  onLeave($event) {
+    const parent = $event.target.parentNode;
+    parent.removeChild(parent.lastChild);
   }
 };
--- src/components/router.jsx
+++ src/components/router.jsx
@@ -28,4 +28,4 @@ export const FccSha = () => (
       </div>
     </div>
   </Router>
-)
+);
